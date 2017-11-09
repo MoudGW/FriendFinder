ad0c2d942a98cc0702dcae88e22a221aa0e311fb
mohamed mahmoud jedeine <moud@mohameds-MacBook-Air.local>
2017-11-09 18:54:54 -0500

    last commit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,7 +2,31 @@ var path = require('path');
 var friends=require('../data/friends.js');
 var f=function(app){
 app.get('/api/friends', function(req, res){
-   res.send(friends.friends);
+   res.json(friends);
 });
+app.post('/api/friends', function(req,res){
+      var user=req.body;
+      var bestmatch;
+      var Topdiff=1000;
+      for(var index in friends.friends)
+      {
+      	var person=friends.friends[index];
+        var diff=friendCalcu(user.scores,person.scores);
+      	if(diff<Topdiff)
+      	{
+      		Topdiff=diff;
+      		bestmatch=person;
+      	}
+      } 
+ res.send(bestmatch);
+}); 
+}
+function friendCalcu(user1,user2){
+var total=0;
+ for (var i=0;i<user1.length;i++)
+ {
+ 	total+=Math.abs(parseInt(user1[i])-parseInt(user2[i]));
+ }
+return total;
 }
 module.exports= {f};
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express=require('express');
 var server=express();
+var bodyParser = require('body-parser')
+server.use( bodyParser.json() );      
+server.use(bodyParser.urlencoded({ extended: true}));
 var app2 =require('./app/routing/apiRoutes.js');
 var app1 =require('./app/routing/htmlRoutes.js');
 app2.f(server);
 app1.f(server);
-
-
 server.listen(3000);
